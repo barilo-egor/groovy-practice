6cd49051785aa89e84151acf0e1feec99be02ab4
barilo <e.barilo@isida.by>
2025-09-08 17:45:15 +0300

    groovy docs

--- src/main/groovy/firstprogramm/HelloWorld.groovy
+++ src/main/groovy/firstprogramm/HelloWorld.groovy
@@ -1,7 +1,14 @@
 package firstprogramm
 
+/**
+ * Groovy class doc
+ */
 class HelloWorld {
 
+    /**
+     * Groovy method doc
+     * @param args
+     */
     static void main(String[] args) {
         print "Hello world!"
     }
